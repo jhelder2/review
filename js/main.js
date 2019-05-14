3bc0753abd7717cd3e7911ae78bfa78864f57aee
Helder <j_helder2@tss-hr420-dm01.fc.ca>
2019-05-14 13:49:33 -0400

    started flexbox lesson

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@
 	// this is a self-invoking function
 	
 	//pythong print -> console.log is the same thing */
-	console.log('status - add - commit - push');
+	console.log('checkout "branchName" = change branch... status - add - commit - push...</p>');
 })();
